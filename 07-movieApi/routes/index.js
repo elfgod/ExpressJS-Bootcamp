00b3246fecd777c5fec390b77e28784e5367422f
ElfGod <albertogx@icloud.com>
2022-04-18 23:39:32 -0500

    pt1 First route and middleware

--- 07-movieApi/routes/index.js
+++ 07-movieApi/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+
+const movies = require('../data/movies')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/most_popular', function (req, res, next) {
+  // get the page variable from the query string
+  let page = req.query.page
+  if (page === undefined) {
+    page = 1
+  }
+  if (req.query.api_key != '123456789') {
+    res.json('invalida API Key')
+  } else {
+    let results = movies.filter((movie) => {
+      return movie.most_popular
+    })
+    const indexToStart = (page - 1) * 20
+    results = results.slice(indexToStart, indexToStart + 19)
+    // res.json(results) // an array
+    res.json({ results }) // an object
+  }
+})
+
+module.exports = router
