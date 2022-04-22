6d4de22d9da6720f42f77733cda61fdee9bccd4d
ElfGod <albertogx@icloud.com>
2022-04-21 23:46:40 -0500

    pt2 Movie Routes and some header work - Continued

--- 07-movieApi/routes/movie.js
+++ 07-movieApi/routes/movie.js
@@ -5,6 +5,22 @@ const movieDetails = require('../data/movieDetails')
 
 /* GET movie page. */
 // /movies/...
+
+// GET /movie/top_rated
+router.get('/top_rated', (req, res, next) => {
+  let page = req.query.page
+  if (!page) {
+    page = 1
+  }
+  const results = movieDetails.sort((a, b) => {
+    return b.vote_average - a.vote_average
+  })
+  const indexToStart = (page - 1) * 20
+  res.json(results.slice(indexToStart, indexToStart + 20))
+})
+
+// GET /movie/movieId
+// This one needs to come last of all /ONETHING
 router.get('/:movieId', (req, res, next) => {
   const movieId = req.params.movieId
   const results = movieDetails.find((movie) => {
@@ -22,10 +38,6 @@ router.get('/:movieId', (req, res, next) => {
     res.json(results)
   }
 })
-
-// GET /movie/movieId
-
-// GET /movie/top_rated
 
 // POST /movie/{movie_id}/rating
 
