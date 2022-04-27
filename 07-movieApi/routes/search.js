f6da19e93c2fed21f935aa6f699f5e0466785a9a
ElfGod <albertogx@icloud.com>
2022-04-26 23:17:58 -0500

    Search Routes with router.use()

--- 07-movieApi/routes/search.js
+++ 07-movieApi/routes/search.js
@@ -1,10 +1,43 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET search page. */
-// /search/...
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+const movies = require('../data/movies')
+const people = require('../data/people')
+
+function queryRequired(req, res, next) {
+  const searchTerm = req.query.query
+  if (!searchTerm) {
+    res.json({ msg: 'Query is required' })
+  } else {
+    next()
+  }
+}
+
+// This middleware will be used by ALL routes in THIS router
+router.use(queryRequired)
+
+// GET /search/movie
+router.get('/movie', (req, res, next) => {
+  const searchTerm = req.query.query
+  const results = movies.filter((movie) => {
+    // found =
+    //  movie.overview.includes(searchTerm) || movie.title.includes(searchTerm)
+    found =
+      movie.overview.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    return found
+  })
+  res.json({ results })
+})
+
+// GET /search/person
+router.get('/person', (req, res, next) => {
+  const searchTerm = req.query.query
+  const results = people.filter((person) => {
+    found = person.name.toLowerCase().includes(searchTerm.toLowerCase())
+    return found
+  })
+  res.json({ results })
 })
 
 module.exports = router
